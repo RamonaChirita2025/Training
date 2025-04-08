c056d780a7f39dd3d303c2575363f25c80e1e696
RamonaChirita2025 <vasile.i.ramona@gmail.com>
2025-04-08 12:38:20 +0300

    Java methods assignment

--- src/main/java/JavaMethodsAssignment.java
+++ src/main/java/JavaMethodsAssignment.java
@@ -11,7 +11,7 @@ public class JavaMethodsAssignment {
         System.out.println(checkPassword("4wrdsedfgyre4fs!"));
         System.out.println(checkPassword("4wrdsedfgyre4fs"));
         System.out.println(checkPassword("wrdsedfLFGgyre4f"));
-        System.out.println(checkPassword("wr6dfvd"));
+        System.out.println(checkPassword("wR6df2d"));
         System.out.println();
 
         System.out.println(wordWithVocals("Dreams"));
@@ -20,10 +20,13 @@ public class JavaMethodsAssignment {
         System.out.println();
 
         System.out.println(generateRandom());
+        System.out.println();
+
         System.out.println(maxOfThreeNumbers(3, 1, 6));
         System.out.println(maxOfThreeNumbers(2, 7, 1));
         System.out.println(maxOfThreeNumbers(5, 1, 4));
         System.out.println(maxOfThreeNumbers(5, 5, 4));
+        System.out.println(maxOfThreeNumbers(1, 3, 3));
         System.out.println(maxOfThreeNumbers(1, 1, 1));
         System.out.println();
 
@@ -34,6 +37,33 @@ public class JavaMethodsAssignment {
         System.out.println(operation("/", 4, 2));
         System.out.println(operation("-", 7, 5));
         System.out.println(operation("+", 2, 4));
+        System.out.println();
+
+        calculateSumOfOddNumber();
+        System.out.println();
+
+        reverseNumber(452);
+        reverseNumber(1943);
+        reverseNumber(20);
+        System.out.println();
+
+        System.out.println(isMagicNumber(102));
+        System.out.println(isMagicNumber(2));
+        System.out.println(isMagicNumber(9));
+        System.out.println();
+
+        isPalindrome("Test");
+        isPalindrome("Hannah");
+        isPalindrome("apa");
+        System.out.println();
+
+        containsCharacter("Tatiana");
+        containsCharacter("number");
+        containsCharacter("test");
+        containsCharacter(" ");
+        System.out.println();
+
+
     }
 
     // 1. verificare parola
@@ -60,7 +90,6 @@ public class JavaMethodsAssignment {
         if (digitsFromPassword.size() < 2) {
             return false;
         }
-
 
         return true;
     }
@@ -131,5 +160,66 @@ public class JavaMethodsAssignment {
         return -1;
     }
 
-}
+    //    additional:
+    //
+    // 1. media aritmetica a tuturor numerelor impare , pana la un numar dat
+    public static void calculateSumOfOddNumber() {
+        int sum = 0;
+        for (int i = 0; i <= 9; i++) {
+            if ((i % 2) != 0) {
+                sum = sum + i;
+            }
+        }
+        System.out.println(sum);
+    }
 
+    // 2. inversul numarului e.g: 12345 -> 54321
+    public static void reverseNumber(int number) {
+        StringBuilder stringBuilder = new StringBuilder(String.valueOf(number));
+        System.out.println(stringBuilder.reverse());
+    }
+    // 3.numar magic
+    public static boolean isMagicNumber (int number) {
+        String numberAsString = String.valueOf(number);
+        int sumOfDigits = 0;
+        for (int i = 0; i < numberAsString.length(); i++) {
+            int digit = Integer.parseInt(String.valueOf(numberAsString.charAt(i)));
+            sumOfDigits = sumOfDigits + digit;
+        }
+        if (number % sumOfDigits == 0 && number/sumOfDigits == 1){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // 4. palindrom
+
+    public static void isPalindrome(String s) {
+        String myString = s, reverseMyString = "";
+        int myStringLength = myString.length();
+        for (int i = (myStringLength - 1); i >= 0; --i) {
+            reverseMyString = reverseMyString + myString.charAt(i);
+        }
+        if (myString.toLowerCase().equals(reverseMyString.toLowerCase())) {
+            System.out.println(myString + " is a palindrome!");
+        } else {
+            System.out.println(myString+ " is not a palindrome!");
+        }
+    }
+
+    // 5. stringul conține un caracter dat si returneaza pozitiile pe care se afla
+    public static void containsCharacter(String myString) {
+        if (myString.contains("t") || myString.contains("T")) {
+            for (int i = 0; i < myString.length(); i++) {
+                char character = myString.charAt(i);
+                if (character == 't' || character == 'T') {
+                    System.out.println(i+1);
+                }
+            }
+        }
+        else{
+            System.out.println("The word doesn't contains 't' character.");
+        }
+    }
+}
